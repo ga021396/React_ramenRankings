f061d9f18e91b3bf4a72b3b6af02cb65ed86c663
ga021396 <ga021396@gmail.com>
2019-03-22 22:12:14 +0800

    add component and styled

---
+++ ramen/src/component/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components'
+
+export function Navbar(){
+    const Root = styled.div`
+        height:100px;
+        width:100px;
+        background:pink;
+    `;
+
+    return(
+        <Root>hello</Root>
+    )
+}
